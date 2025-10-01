abc98d6781787e05e33da5560c65b93e75ae8016
abdelrahman mohamed <97918409+abdelrahman1mohamed1@users.noreply.github.com>
2025-10-01 21:10:51 +0300

    updated loading components

---
+++ app/[locale]/feed/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-teal-400"></div>
+    </div>
+  );
+}
